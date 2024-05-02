09b34e8db5e7289335b4ee1cb83acf492ccc7566
Alma Yovell <alma.yovell@gmail.com>
2024-05-02 19:11:42 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div>
+        <open-screen v-if="this.page === 0"  @next-page='nextPage'> </open-screen>
+        <home-page v-if="this.page === 1"></home-page>
+    </div>
+  </div>
+</template>
+ 
+<script>
+import openScreen from "@/components/openScreen.vue";
+import homePage from "@/components/homePage.vue";
+
+export default {
+  name: "app",
+  components: {
+    openScreen,
+    homePage
+  },
+  data() {
+    return {
+      page: 0,
+    };
+  },
+  methods: {
+    nextPage() {
+      this.page++;
+    },
+  },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: Karantina;
+  src: url("assets/fonts/Karantina-Regular.ttf");
+}
+
+@font-face {
+  font-family: Assistant;
+  src: url("assets/fonts/Assistant-VariableFont_wght.ttf");
+}
+
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+  direction: rtl;
+}
+
+</style>
---
+++ src/components/homePage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="home-page">
+        <img :src="require('@/assets/imgs/collegeLogo.png')" alt="logo" id="logo" />
+
+    </div>
+</template>
+   
+<script>
+export default {
+    name: "home-page",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {
+        nextPage() {
+            this.$emit('next-page');
+        },
+    },
+};
+</script>
+  
+<style scoped>
+#home-page {
+    height: 100vh;
+    width: 100vw;
+    background-image: url("../assets/imgs/Bg2.png");
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    overflow: hidden;
+}
+
+#logo {
+    position: absolute;
+    top: 3vh;
+    left: 34vh;
+    width: 20vh;
+}
+</style>
